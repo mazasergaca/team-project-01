847307c71a9e045686c4ddb18dab2bb43daa2d81
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-02-03 21:46:20 +0200

    autoplay for comments

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,6 +1,11 @@
- var swiper = new Swiper('.mySwiper', {
+var swiper = new Swiper('.mySwiper', {
+  autoplay: {
+delay: 2500, disableOnInteraction: false,
+  },
+      loop: true,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
+      
     });
