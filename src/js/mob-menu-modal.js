f66a0341d8c57e946e72bae89e8a521f85d826f7
Miroslav34 <miroslavshimchuk@gmail.com>
2022-02-04 11:37:46 +0200

    button modal form done

--- src/js/mob-menu-modal.js
+++ src/js/mob-menu-modal.js
@@ -1,15 +1,20 @@
-// (() => {
-//     const refs = {
-//       openModalBtn: document.querySelector('.header-button-buy'),
-//       closeModalBtn: document.querySelector('[data-modal-close]'),
-//       modal: document.querySelector('[data-modal]'),
-//     };
+(() => {
+    const refs = {
+      openModalBtn1: document.querySelector('.header-button-buy'),
+      closeModalBtn1: document.querySelector('.modal__button'),
+      modal1: document.querySelector('[data-modal]'),
+    };
   
-//     refs.openModalBtn.addEventListener('click', toggleModal);
-//     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtn1.addEventListener('click', addModal1);
+    refs.closeModalBtn1.addEventListener('click', removeModal1);
   
-//     function toggleModal() {
-//       document.body.classList.toggle("modal-open");
-//       refs.modal.classList.toggle('is-hidden');
-//     }
-//   })();
+    function removeModal1() {
+      document.body.classList.remove("modal-open");
+      refs.modal1.classList.add('is-hidden');
+    }
+
+    function addModal1() {
+      document.body.classList.add("modal-open");
+      refs.modal1.classList.remove('is-hidden');
+    }
+  })();
